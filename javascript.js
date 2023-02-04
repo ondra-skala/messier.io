9d86e15708b5fe2dc86c0b08c332379038f25535
ondra-skala <93881855+ondra-skala@users.noreply.github.com>
2023-02-04 21:22:57 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -18,11 +18,37 @@ var m97= "https://upload.wikimedia.org/wikipedia/commons/1/10/The_Owl_Nebula_M97
 var m101= "https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/m101.jpg?itok=CDlepVkh";
 
 
+const imagesEasy = [["m1","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/crab-nebula-mosaic.jpg?itok=DNaRmPtc"],
+["m13","https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Messier_13.jpg/1200px-Messier_13.jpg"],
+["m31","https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Andromeda_Galaxy_%28with_h-alpha%29.jpg/1200px-Andromeda_Galaxy_%28with_h-alpha%29.jpg"],
+["m20","https://www.astro.cz/images/cam/2016_05/obr/2016_2q_05_06.jpg"],
+["m42","http://cs.astronomy.com/cfs-file.ashx/__key/telligent-evolution-components-attachments/13-59-00-00-00-48-84-46/m42_2D00_HLRGB_5F00_def.jpg"],
+["m45","https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Pleiades_large.jpg/1200px-Pleiades_large.jpg"],
+["m57","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/ring-nebula-full_jpg.jpg?itok=yRmKArDB"],
+["m81","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/m81-print.jpg?itok=cQ8vTd7C"],
+["m104","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/sombrero-galaxy.jpg?itok=0bph5fym"],
+["m87","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/m87-full_jpg.jpg?itok=XqvbUq_t"],
+]
 
 const imageArray = [["m1","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/crab-nebula-mosaic.jpg?itok=DNaRmPtc"],
 ["m13","https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Messier_13.jpg/1200px-Messier_13.jpg"],
 ["m16","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/hubble_birthofstars_0.jpg?itok=V2crHtH0"],
-["m31","https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Andromeda_Galaxy_%28with_h-alpha%29.jpg/1200px-Andromeda_Galaxy_%28with_h-alpha%29.jpg"]
+["m24","https://upload.wikimedia.org/wikipedia/commons/c/c4/Messier_24_Colombari_crop_invert.jpg"],
+["m27","https://upload.wikimedia.org/wikipedia/commons/7/7a/M27-Mazur.jpg"],
+["m63","https://upload.wikimedia.org/wikipedia/commons/0/0f/M63s.jpg"],
+["m74","https://www.popsci.com/uploads/2022/08/29/M74-Galaxy.jpeg?auto=webp"],
+["m76","https://upload.wikimedia.org/wikipedia/commons/f/f0/M76-RL5-DDmin-Gamma-LRGB_883x628.jpg"],
+["m77","https://upload.wikimedia.org/wikipedia/commons/f/f5/M77_Galaxy_from_the_Mount_Lemmon_SkyCenter_Schulman_Telescope_courtesy_Adam_Block.jpg"],
+["m78","https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Messier_78.jpg/1024px-Messier_78.jpg"],
+["m83","https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/M83_-_Southern_Pinwheel.jpg/1024px-M83_-_Southern_Pinwheel.jpg"],
+["m88","https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/M88s.jpg/1280px-M88s.jpg"],
+["m94","https://www.messier-objects.com/wp-content/uploads/2015/08/Messier-94.jpg"],
+["m61","https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Messier61_-_ESO_-_Potw1901a.tif/lossy-page1-1024px-Messier61_-_ESO_-_Potw1901a.tif.jpg"],
+["m30","https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Messier_30_Hubble_WikiSky.jpg/1024px-Messier_30_Hubble_WikiSky.jpg"],
+["m7","https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/The_star_cluster_Messier_7.jpg/1024px-The_star_cluster_Messier_7.jpg"],
+["m58","https://upload.wikimedia.org/wikipedia/commons/b/b0/M58s_%28visible%29.jpg"],
+["m17","https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/The_star_formation_region_Messier_17.jpg/1024px-The_star_formation_region_Messier_17.jpg"]
+["m31","https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Andromeda_Galaxy_%28with_h-alpha%29.jpg/1200px-Andromeda_Galaxy_%28with_h-alpha%29.jpg"],
 ["m33","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/heic1901a.jpg?itok=YLJUZyay"],
 ["m42","http://cs.astronomy.com/cfs-file.ashx/__key/telligent-evolution-components-attachments/13-59-00-00-00-48-84-46/m42_2D00_HLRGB_5F00_def.jpg"],
 ["m51","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/m51-and-companion_0.jpg?itok=OPfxPWWf"],
@@ -36,7 +62,10 @@ const imageArray = [["m1","https://www.nasa.gov/sites/default/files/styles/full_
 ["m20","https://www.astro.cz/images/cam/2016_05/obr/2016_2q_05_06.jpg"],
 ["m104","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/sombrero-galaxy.jpg?itok=0bph5fym"],
 ["m97","https://upload.wikimedia.org/wikipedia/commons/1/10/The_Owl_Nebula_M97_Goran_Nilsson_%26_The_Liverpool_Telescope.jpg"],
-["m101","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/m101.jpg?itok=CDlepVkh"]
+["m101","https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/m101.jpg?itok=CDlepVkh"],
+["m102","https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Ngc5866_hst_big.png/800px-Ngc5866_hst_big.png"],
+["m109","https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Messier109_-_SDSS_DR14_%28panorama%29.jpg/1280px-Messier109_-_SDSS_DR14_%28panorama%29.jpg"],
+["m108","https://noirlab.edu/public/media/archives/images/large/noao-m108.jpg"]
 ];
 
 
@@ -47,6 +76,7 @@ const image = document.querySelector("img");
 
 var pozice = 0;
 var num = 0;
+var stav = 0;
 
 function generateRandomPicture(array){
   var randomNum = Math.floor(Math.random() * array.length);
@@ -56,22 +86,57 @@ function generateRandomPicture(array){
 }
 
 function myFunction() {
-  
+  if (stav == 1){
+    FunEasy();
+  }
+  if (stav == 2){
+    FunHard();
+  }  
+  if (stav == 0){
+    alert("Prosím vyberte obtížnost!");
+  }
+}
 
+function FunEasy(){
+  stav = 0;
+
+  document.getElementById("smazat1").style.display = "none";
+  document.getElementById("smazat2").style.display = "none";
+  document.getElementById("smazat3").style.display = "none";
+
+
+  const image = document.querySelector("img");
+
+  const num = generateRandomPicture(imagesEasy);
+  window.onload = () => num;
+  pozice = imagesEasy[num];
+  
+  stav = 1;
+}
+
+function FunHard(){
+  stav = 0;
+
+  document.getElementById("smazat1").style.display = "none";
+  document.getElementById("smazat2").style.display = "none";
+  document.getElementById("smazat3").style.display = "none";
   const image = document.querySelector("img");
 
   const num = generateRandomPicture(imageArray);
   window.onload = () => num;
   pozice = imageArray[num];
 
-  
+  stav = 2;
 }
 
 
 function check(){
+  if (stav == 0){
+    alert("Prosím vyberte obtížnost!");
+  }
   let text;
   let x = document.getElementById("numb").value;
-  if (x === pozice[0]) {
+  if (x == pozice[0]) {
     text = "✅";
     document.getElementById("demo").innerHTML = text;
     myFunction();
@@ -80,9 +145,16 @@ function check(){
     text = "🚫";
     document.getElementById("demo").innerHTML = text;
   }
-  
+  let frm = document.getElementById("numb");
+  frm.value = "m";
 }
 
 function idk(){
-  alert(pozice[0])
+  if (stav == 0){
+    alert("Prosím vyberte obtížnost!");
+  }
+  else{
+    alert(pozice[0]);
+  }
+  
 }
